ae3bea02d988bb6ee00acd62604eeacf315d14fe
tuanduongcam <tuanduongcam@outlook.com>
2018-05-14 09:36:52 +0700

    add missing files

---
+++ EventManagerApp/Models/CityModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventManager.Web.Models
+{
+	public class CityModel
+	{
+		public int CityID { get; set; }
+		public string Name { get; set; }
+	}
+}
