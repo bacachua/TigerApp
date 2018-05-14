5119df4ca05a5b40d95fefeeeb94f58d1e0ab859
Nguyen van Chau <nvchau.niit@gmail.com>
2018-05-14 20:12:44 +0700

    move bussiness to service.

--- EventManager.BusinessService/EventCampaignBusinessService.cs
+++ EventManager.BusinessService/EventCampaignBusinessService.cs
@@ -9,49 +9,57 @@ using EventManager.Repository;
 using Repository.Pattern;
 using Repository.Pattern.Repositories;
 using EventManager.ApiModels;
+using Repository.Pattern.DataContext;
+using Repository.Pattern.UnitOfWork;
+using Repository.Pattern.Ef6;
+
 namespace EventManager.BusinessService
 {
-    public interface IEventCampaignBusinessService : IService<EventCampaign>
+    public interface IEventCampaignBusinessService
     {
         List<ApiEventCampaignModel> GetListAvailable();
     }
 
-    public class EventCampaignBusinessService : Service<EventCampaign>, IEventCampaignBusinessService
+    public class EventCampaignBusinessService : IEventCampaignBusinessService
     {
-        private readonly IRepositoryAsync<EventCampaign> _repository;
-        public EventCampaignBusinessService(IRepositoryAsync<EventCampaign> repository)
-            : base(repository)
+        private IRepositoryAsync<EventCampaign> _repository;
+        public EventCampaignBusinessService()
         {
-            _repository = repository;
+
         }
 
         public List<ApiEventCampaignModel> GetListAvailable()
         {
             List<ApiEventCampaignModel> models = new List<ApiEventCampaignModel>();
-            var currentTime = DateTime.Now;
-            var entities = _repository.AllIncluding(c => c.City, c => c.Event, c=>c.EventRegisters).Where(c => c.StartDateTime >= currentTime).OrderBy(c=>c.EventCampaignID).OrderBy(c=>c.StartDateTime).ToList();
-            models = entities.Select(c => new ApiEventCampaignModel()
+            using (IDataContextAsync context = new GameManagerContext())
+            using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
             {
-                EventCampaignID = c.EventCampaignID,
-                EventName = c.Event.Name,
-                CityName = c.City.Name,
-                StartDateTime = c.StartDateTime,
-                EndDateTime = c.EndDateTime,
-                TimeToPlayPerSession = c.TimeToPlayPerSession,
-                NumberOfPlayer1Time = c.NumberOfPlayer1Time
-            }).ToList();
-            foreach (var item in models)
-            {
-                var entity = entities.FirstOrDefault(c => c.EventCampaignID == item.EventCampaignID);
-                var startTime = new DateTime(item.StartDateTime.Value.Year, item.StartDateTime.Value.Month, item.StartDateTime.Value.Day, item.StartDateTime.Value.Hour, item.StartDateTime.Value.Minute, 0);
-                while (startTime <= item.EndDateTime.Value)
-                {                    
-                    if (!entity.EventRegisters.Any(c => c.StartDateTime.Value <= startTime && startTime < c.EndDateTime.Value))
+                _repository = new Repository<EventCampaign>(context, unitOfWork);                
+                var currentTime = DateTime.Now;
+                var entities = _repository.AllIncluding(c => c.City, c => c.Event, c => c.EventRegisters).Where(c => c.StartDateTime >= currentTime).OrderBy(c => c.EventCampaignID).OrderBy(c => c.StartDateTime).ToList();
+                models = entities.Select(c => new ApiEventCampaignModel()
+                {
+                    EventCampaignID = c.EventCampaignID,
+                    EventName = c.Event.Name,
+                    CityName = c.City.Name,
+                    StartDateTime = c.StartDateTime,
+                    EndDateTime = c.EndDateTime,
+                    TimeToPlayPerSession = c.TimeToPlayPerSession,
+                    NumberOfPlayer1Time = c.NumberOfPlayer1Time
+                }).ToList();
+                foreach (var item in models)
+                {
+                    var entity = entities.FirstOrDefault(c => c.EventCampaignID == item.EventCampaignID);
+                    var startTime = new DateTime(item.StartDateTime.Value.Year, item.StartDateTime.Value.Month, item.StartDateTime.Value.Day, item.StartDateTime.Value.Hour, item.StartDateTime.Value.Minute, 0);
+                    while (startTime <= item.EndDateTime.Value)
                     {
-                        item.TimeAvailableToPlay = startTime;
-                        break;
+                        if (!entity.EventRegisters.Any(c => c.StartDateTime.Value <= startTime && startTime < c.EndDateTime.Value))
+                        {
+                            item.TimeAvailableToPlay = startTime;
+                            break;
+                        }
+                        startTime = startTime.AddMinutes(item.TimeToPlayPerSession.Value);
                     }
-                    startTime = startTime.AddMinutes(item.TimeToPlayPerSession.Value);
                 }
             }
             return models;
--- EventManagerApp/Controllers/EventCampaignController.cs
+++ EventManagerApp/Controllers/EventCampaignController.cs
@@ -24,77 +24,7 @@ namespace EventManager.Web.Controllers
 {
 	
     public class EventCampaignController : ApiController
-    {
-        // GET: api/EventCampaig
-        [AllowAnonymous]
-		[HttpGet]
-		public HttpResponseMessage Get()
-        {
-			List<EventCampaignModel> eventCampaignList = null;
-			using (IDataContextAsync context = new GameManagerContext())
-			using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
-			{
-				IRepositoryAsync<EventCampaign> campaignRepository = new Repository<EventCampaign>(context, unitOfWork);
-				IEventCampaignBusinessService billingService = new EventCampaignBusinessService(campaignRepository);
-
-				IQueryFluent<EventCampaign> q = campaignRepository.Query(x => x.EventCampaignID > 0);
-				eventCampaignList = q.Select(y => new EventCampaignModel { EventCampaignID = y.EventCampaignID, EventID = y.EventID, EventName = y.Event.Name, CityID = y.City.CityID, CityName = y.City.Name, StartDateTime = y.StartDateTime, EndDateTime = y.EndDateTime, TimeToPlayPerSession = y.TimeToPlayPerSession, NumberOfPlayer1Time = y.NumberOfPlayer1Time, Active = y.Active, WaitingTime = 0 }).ToList();
-				//eventCampaignList = q.SelectAsync().Result.ToList();
-				//(t => new EventCampaignModel { EventCampaignID  = q.EventCampaignID});
-				//eventCampaignList = asyncTask.Result.Select( new EventCampaignModel{ }};
-				
-				if (eventCampaignList == null)
-				{
-					var myError = new Error
-					{
-						Status = Resources.ApiMsg.Failed,
-						Message = Resources.ApiMsg.NoRecordFound
-					};
-
-					return Request.CreateResponse(HttpStatusCode.OK, myError);					
-				}
-				else
-				{
-					return Request.CreateResponse(HttpStatusCode.OK, eventCampaignList);
-				}
-			}
-        }
-
-        // GET: api/EventCampaign/5
-		[AllowAnonymous]
-		[HttpGet]
-		public HttpResponseMessage Get(int id)
-        {
-           	EventCampaignModel eventCampaign = null;
-			using (IDataContextAsync context = new GameManagerContext())
-			using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
-			{
-				IRepositoryAsync<EventCampaign> campaignRepository = new Repository<EventCampaign>(context, unitOfWork);
-				IEventCampaignBusinessService billingService = new EventCampaignBusinessService(campaignRepository);
-
-				IQueryFluent<EventCampaign> q = campaignRepository.Query(x => x.EventCampaignID == id);
-				eventCampaign = q.Select(y => new EventCampaignModel { EventCampaignID = y.EventCampaignID, EventID = y.EventID,EventName = y.Event.Name, CityID = y.City.CityID, CityName = y.City.Name, StartDateTime = y.StartDateTime, EndDateTime = y.EndDateTime, TimeToPlayPerSession = y.TimeToPlayPerSession, NumberOfPlayer1Time = y.NumberOfPlayer1Time, Active = y.Active, WaitingTime = 0 }).FirstOrDefault();
-				//eventCampaignList = q.SelectAsync().Result.ToList();
-				//(t => new EventCampaignModel { EventCampaignID  = q.EventCampaignID});
-				//eventCampaignList = asyncTask.Result.Select( new EventCampaignModel{ }};
-
-				if (eventCampaign == null)
-				{
-					var myError = new Error
-					{
-						Status = Resources.ApiMsg.Failed,
-						Message = Resources.ApiMsg.NoRecordFound
-					};
-					return Request.CreateResponse(HttpStatusCode.OK, myError);	
-				}
-				else
-				{
-					return Request.CreateResponse(HttpStatusCode.OK, eventCampaign);
-				}
-			}
-        }
-
-		
+    {		
         // POST: api/EventCampaign
         public void Post([FromBody]string value)
         {
@@ -114,14 +44,8 @@ namespace EventManager.Web.Controllers
         [HttpGet]
         public APIResponse GetListAvailable()
         {
-            var result = new List<ApiEventCampaignModel>();
-            using (IDataContextAsync context = new GameManagerContext())
-            using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
-            {
-                IRepositoryAsync<EventCampaign> campaignRepository = new Repository<EventCampaign>(context, unitOfWork);
-                IEventCampaignBusinessService eVentCampaignSrv = new EventCampaignBusinessService(campaignRepository);
-                result = eVentCampaignSrv.GetListAvailable();
-            }
+            IEventCampaignBusinessService eVentCampaignSrv = new EventCampaignBusinessService();
+            var result = eVentCampaignSrv.GetListAvailable();
             return new APIResponse() { Status = eResponseStatus.Success, Result = result };
         }
     }
