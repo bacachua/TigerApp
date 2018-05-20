21facaabfd4856434bc2023b6bc0125e939407f1
tuanduongcam@outlook.com <tuanduongcam@outlook.com>
2018-05-20 22:21:04 +0700

    commit

--- EventManager.ApiModels/ApiEventCampaignModel.cs
+++ EventManager.ApiModels/ApiEventCampaignModel.cs
@@ -9,8 +9,7 @@ namespace EventManager.ApiModels
     public class ApiEventCampaignModel
     {
         public int EventCampaignID { get; set; }
-        public string EventName { get; set; }
-
+		public string EventName { get; set; }
 		public int EventID { get; set; }
         public string CityName { get; set; }
 		public Nullable<int> CityID { get; set; }
